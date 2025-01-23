bfcc9d0aab1ca901c7a03234331591920029d097
Dilith Achalan <dilith.achalan@outlook.com>
2025-01-23 15:17:53 +0530

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/",
+  base: "/LearnTrade_Web",
   optimizeDeps: {
     exclude: ["lucide-react"],
   },
