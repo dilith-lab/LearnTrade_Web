3a5330b3b920c847d642fdf619a2e7f297c20161
Dilith Achalan <dilith.achalan@outlook.com>
2025-01-23 15:34:19 +0530

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 import Features from "./components/Features";
